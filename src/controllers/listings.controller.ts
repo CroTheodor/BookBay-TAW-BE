b340e0935b47b3ff56d7f0a65c8aedc8c67f8209
Theo <cro.theodor@gmail.com>
2024-06-27 20:09:55 +0200

    get all listings

--- src/controllers/listings.controller.ts
+++ src/controllers/listings.controller.ts
@@ -19,3 +19,46 @@ export const listingCreate = (req, res)=>{
         (l: listing.ListingDTO)=>res.status(200).json(new HttpResponse(true, "Listing created: "+l.id, l))
     ).catch(()=>res.status(500).json(new HttpResponse(false, "DB error", null)))
 }
+
+export const listingGetAll = async (req,res)=>{
+
+    const titleFilter = req.query.title;
+    const authorFilter = req.query.author;
+    const publisherFilter = req.query.publisher;
+    const courseFilter = req.query.course;
+
+    let filter = {};
+
+    if(titleFilter){
+        filter = {...filter, "title":req.query.title};
+    }
+
+    if(authorFilter){
+        filter = {...filter, "author":req.query.author};
+    }
+
+    if(publisherFilter){
+        filter = {...filter, "publisher":req.query.publisher}
+    }
+
+    if(courseFilter){
+        filter = {...filter, "course":req.query.course};
+    }
+
+    const page = req.query.page;
+    const pageNumber = page ? Number.parseInt(page) : 1;
+    const limit = page ? req.query.limit : 50000;
+    const numberOfDocuments = await ListingModel.countDocuments().exec();
+    const totalPages = numberOfDocuments / limit;
+
+    ListingModel.find(filter)
+            .limit(limit)
+            .skip(pageNumber - 1)
+            .sort({listingDate: -1})
+            .populate("postingUser")
+            .populate("bidingUser")
+            .then(
+                (el)=>console.log(el)
+        )
+
+}
--- src/routes/listings.route.ts
+++ src/routes/listings.route.ts
@@ -1,11 +1,11 @@
 import express from "express";
 import { auth } from "../middleware/auth";
 import { isModerator } from "../middleware/isModerator";
-import { listingCreate,  } from "../controllers/listings.controller";
+import { listingCreate, listingGetAll,  } from "../controllers/listings.controller";
 
 const router = express.Router();
 router.route('')
     .post(auth,listingCreate)
-    // .get(auth,listingGetAll)
+    .get(auth,listingGetAll)
 
 module.exports = router;
