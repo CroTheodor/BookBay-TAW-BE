db454d6e0438e0e8b2e0e8a63b5f2dce819f426e
Theo <cro.theodor@gmail.com>
2024-07-22 13:00:08 +0200

    messages model and controller wip

---
+++ src/models/messages.model.ts
@@ -0,0 +1,130 @@
+import { model, Model, Schema, SchemaTypes } from "mongoose";
+
+export interface PublicMessageDTO{
+    _id: Schema.Types.ObjectId;
+    userId: Schema.Types.ObjectId;
+    userName: string;
+    userLastname: string;
+    date: string;
+    content: string;
+}
+
+export interface PrivateMessageDTO{
+    _id: Schema.Types.ObjectId;
+    date: string;
+    content: string;
+}
+
+export interface PublicChatroomDTO{
+    _id: Schema.Types.ObjectId;
+    chatroomId: string;
+    messages: PublicMessageDTO[];
+}
+
+export interface PrivateChatroomDTO{
+    _id: Schema.Types.ObjectId;
+    chatroomId: string;
+    user1:Schema.Types.ObjectId;
+    user2:Schema.Types.ObjectId;
+    messages: PrivateMessageDTO[];
+}
+
+const privateMessage = new Schema<PrivateMessageDTO>({
+    date: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    content: {
+        type: SchemaTypes.String,
+        required: true
+    },
+})
+
+const publicMessage = new Schema<PublicMessageDTO>({
+    date: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    content: {
+        type: SchemaTypes.String,
+        required: true
+    },
+    userId:{
+        type: SchemaTypes.ObjectId,
+        required: true,
+    },
+    userName:{
+        type: SchemaTypes.String,
+        required: true,
+    },
+    userLastname:{
+        type: SchemaTypes.String,
+        required: true,
+    },
+})
+
+const publicChatroom = new Schema<PublicChatroomDTO>({
+    chatroomId:{
+        type: SchemaTypes.String,
+        required:true,
+    },
+    messages:{
+        type: [publicMessage],
+        required:true,
+    }
+})
+
+const privateChatroom = new Schema<PrivateChatroomDTO>({
+    chatroomId:{
+        type: SchemaTypes.String,
+        required:true,
+    },
+    messages:{
+        type: [publicMessage],
+        required:true,
+    },
+    user1:{
+        type: SchemaTypes.ObjectId,
+        required: true,
+    },
+    user2:{
+        type: SchemaTypes.ObjectId,
+        required: true,
+    }
+})
+
+export function getPrivateChatroomSchema() {
+    return privateChatroom;
+}
+
+export function getPublicChatroomSchema() {
+    return publicChatroom;
+}
+
+let privateChatroomModel: any;
+export function getPrivateChatroomModel(): Model<PrivateChatroomDTO> {
+    if (!privateChatroomModel) {
+        privateChatroomModel = model('PrivateChatroom', getPrivateChatroomSchema());
+    }
+    return privateChatroomModel;
+}
+
+export function newPrivateChatroom(data: any): PrivateChatroomDTO {
+    let _privateChatroomModel = privateChatroomModel();
+    let privateChatroom = new _privateChatroomModel(data);
+    return privateChatroom;
+}
+
+let publicChatroomModel: any;
+export function getPublicChatroomModel(): Model<PublicChatroomDTO> {
+    if (!publicChatroomModel) {
+        publicChatroomModel = model('PublicChatroom', getPublicChatroomSchema());
+    }
+    return publicChatroomModel;
+}
+
+export function newPublicChatroom(data: any): PublicChatroomDTO {
+    let _publicChatroomModel = getPublicChatroomModel();
+    let publicChatroom = new _publicChatroomModel(data);
+    return publicChatroom;
+}
